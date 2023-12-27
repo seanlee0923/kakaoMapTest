ea2180e1db0adfe1d2d4fb651e4bfc1cae413805
seanlee0923 <leesinue0923@gmail.com>
2023-12-27 15:52:42 +0900

    프로젝트 생성

---
+++ src/test/java/com/kakaomap/kakaomaptest/KakaomaptestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kakaomap.kakaomaptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaomaptestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
